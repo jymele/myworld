6f9422041aef7120dd4fb988082ee14e617ccc5b
Joel Ymele <jymele@yahoo.fr>
2025-03-06 00:52:41 -0500

    Added the keyboard controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { KeyboardControls } from "@react-three/drei";
 import "./App.css";
 import Experience from "./game/Experience";
+import { useMemo } from "react";
 
 function App() {
+  const map = useMemo(
+    () => [
+      { name: "forward", keys: ["ArrowUp", "KeyW"] },
+      { name: "back", keys: ["ArrowDown", "KeyS"] },
+      { name: "left", keys: ["ArrowLeft", "KeyA"] },
+      { name: "right", keys: ["ArrowRight", "KeyD"] },
+      { name: "jump", keys: ["Space"] },
+    ],
+    []
+  );
+
   return (
     <>
-      <Experience />
+      <KeyboardControls map={map}>
+        <Experience />
+      </KeyboardControls>
     </>
   );
 }
--- src/game/controllers/PlayerController.jsx
+++ src/game/controllers/PlayerController.jsx
@@ -1,9 +1,25 @@
 import { CapsuleCollider, RigidBody } from "@react-three/rapier";
 import Player from "../models/Player";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { useKeyboardControls } from "@react-three/drei";
 
 export default function PlayerController() {
+  const rb = useRef();
+  const [, get] = useKeyboardControls();
+
+  useFrame(() => {
+    const { forward, backward, left, right, jump } = get();
+
+    if (rb.current) {
+      if (forward) {
+        console.log("forward");
+      }
+    }
+  });
+
   return (
-    <RigidBody colliders={false} position={[0, 2, 0]}>
+    <RigidBody ref={rb} colliders={false} position={[0, 2, 0]}>
       <CapsuleCollider args={[1, 1.05]} />
       <Player />
     </RigidBody>
