93976376f450502960cd93cb20289da3b21561bc
Joel Ymele <jymele@yahoo.fr>
2025-03-06 01:01:12 -0500

    Few modifs

--- src/game/controllers/PlayerController.jsx
+++ src/game/controllers/PlayerController.jsx
@@ -3,23 +3,42 @@ import Player from "../models/Player";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { useKeyboardControls } from "@react-three/drei";
+import { Vector3 } from "three";
 
 export default function PlayerController() {
   const rb = useRef();
   const [, get] = useKeyboardControls();
+  const movementSpeed = 10;
+  const vel = new Vector3();
 
   useFrame(() => {
-    const { forward, backward, left, right, jump } = get();
+    vel.x = 0;
+    vel.y = -1;
+    vel.z = 0;
+
+    const { forward, back, left, right } = get();
 
     if (rb.current) {
       if (forward) {
-        console.log("forward");
+        vel.z = -1 * movementSpeed;
       }
+      if (back) {
+        vel.z = 1 * movementSpeed;
+      }
+
+      if (left) {
+        vel.x = -1 * movementSpeed;
+      }
+      if (right) {
+        vel.x = 1 * movementSpeed;
+      }
+
+      rb.current.setLinvel(vel);
     }
   });
 
   return (
-    <RigidBody ref={rb} colliders={false} position={[0, 2, 0]}>
+    <RigidBody ref={rb} colliders={false} position={[0, 2.4, 0]} lockRotations>
       <CapsuleCollider args={[1, 1.05]} />
       <Player />
     </RigidBody>
