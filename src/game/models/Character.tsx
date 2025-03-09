7736816c67f9fc5bf64b353c79ca15b8bb307eb8
Joel Ymele <joel.ymele@gmail.com>
2025-03-08 21:05:20 -0500

    Added material to the player

--- src/game/models/Character.tsx
+++ src/game/models/Character.tsx
@@ -6,14 +6,12 @@ import { GLTF } from "three-stdlib";
 type GLTFResult = GLTF & {
   nodes: {
     Cube004: THREE.SkinnedMesh;
-    Cube005: THREE.SkinnedMesh;
-    Cube006: THREE.SkinnedMesh;
-    Cube007: THREE.SkinnedMesh;
     Bone_1: THREE.Bone;
     Bone002_1: THREE.Bone;
     Bone005_1: THREE.Bone;
     Bone003_1: THREE.Bone;
     Bone004_1: THREE.Bone;
+    neutral_bone: THREE.Bone;
   };
   materials: {
     Material: THREE.MeshStandardMaterial;
@@ -54,29 +52,12 @@ export function Character(props: Props) {
             material={materials.Material}
             skeleton={nodes.Cube004.skeleton}
           />
-          <skinnedMesh
-            name="Cube005"
-            geometry={nodes.Cube005.geometry}
-            material={materials.Material}
-            skeleton={nodes.Cube005.skeleton}
-          />
-          <skinnedMesh
-            name="Cube006"
-            geometry={nodes.Cube006.geometry}
-            material={materials.Material}
-            skeleton={nodes.Cube006.skeleton}
-          />
-          <skinnedMesh
-            name="Cube007"
-            geometry={nodes.Cube007.geometry}
-            material={materials.Material}
-            skeleton={nodes.Cube007.skeleton}
-          />
           <primitive object={nodes.Bone_1} />
           <primitive object={nodes.Bone002_1} />
           <primitive object={nodes.Bone005_1} />
           <primitive object={nodes.Bone003_1} />
           <primitive object={nodes.Bone004_1} />
+          <primitive object={nodes.neutral_bone} />
         </group>
       </group>
     </group>
